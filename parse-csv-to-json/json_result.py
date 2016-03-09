9ed4b914b13b434ca1032e506a3f8ffa7d2b7036
saranya <saranyaks515@gmail.com>
2016-03-09 12:20:49 +0530

    code changed to older version 2.6

--- parse-csv-to-json/json_result.py
+++ parse-csv-to-json/json_result.py
@@ -1,6 +1,6 @@
 import csv
 import json
-with open('datafile1.csv', 'r') as f:
+with open('datafile.csv', 'r') as f:
 	reader = csv.DictReader(f)
 	out = [obj for obj in reader]
 with open("result3.json", "w") as f:
